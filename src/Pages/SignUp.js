c554e9203dd757c5ecad9c40ff3c40248ba5a10d
Shashank G Acharya <shashuviper21@gmail.com>
2022-07-29 16:47:31 +0530

    register page

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,10 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import SignUpComponent from "../components/SignUpComponent";
 
 const SignUp = () => {
+  let [passwordType, setPasswordType] = useState("password");
+
+  const togglePassword = () => {
+    if (passwordType == "password") {
+      setPasswordType("text");
+      return;
+    }
+    setPasswordType("password");
+  };
   return (
     <>
-      <SignUpComponent>SignUp</SignUpComponent>
+      <SignUpComponent>
+        <section className="h-full">
+          <div className="px-6 py-6 h-full text-gray-800">
+            <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+              <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                  className="w-full"
+                  alt="Sample image"
+                />
+              </div>
+              <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+                <div className="mt-2 container px-8 py-4 rounded-xl bg-blue-500/25">
+                  <form>
+                    <div className="form-group mb-6">
+                      <label className="flex justify-center font-bold text-xl">
+                        Register Form
+                      </label>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div className="form-group mb-6">
+                        <input
+                          type="text"
+                          className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="First name"
+                        />
+                      </div>
+                      <div className="form-group mb-6">
+                        <input
+                          type="text"
+                          className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Last name"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group mb-6">
+                      <input
+                        type="email"
+                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        id="exampleInput125"
+                        placeholder="Email address"
+                      />
+                    </div>
+                    <div className="form-group mb-6">
+                      <input
+                        type="password"
+                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="Password"
+                      />
+                    </div>
+                    <div className="form-group mb-6">
+                      <input
+                        type="password"
+                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="Confirm Password"
+                      />
+                      {passwordType === "password" ? (
+                        <a
+                          className="float-right font-bold text-blue-600 hover:text-black focus:text-black transition duration-200 ease-in-out"
+                          onClick={togglePassword}
+                        >
+                          Show Password
+                        </a>
+                      ) : (
+                        <a
+                          className="float-right font-bold text-blue-600 hover:text-black focus:text-black transition duration-200 ease-in-out"
+                          onClick={togglePassword}
+                        >
+                          Hide Password
+                        </a>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div className="form-group mb-6">
+                        <input
+                          type="number"
+                          className="form-control block w-full mt-3 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Mobile Number"
+                        />
+                      </div>
+                      <div className="form-group mb-6">
+                        <input
+                          type="text"
+                          className="form-control block w-full mt-3 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="City"
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group mb-6">
+                      <textarea
+                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        rows="3"
+                        placeholder="Address"
+                      ></textarea>
+                    </div>
+                    <button
+                      type="submit"
+                      className="w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    >
+                      Sign up
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </SignUpComponent>
     </>
   );
 };
